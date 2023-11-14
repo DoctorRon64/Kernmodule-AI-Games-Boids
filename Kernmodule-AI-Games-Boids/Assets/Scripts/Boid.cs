2a76dfc0b7de4b9a2152fd75ab4f2425eb1c4776
DoctorRon64 <piepmail@lefthandmedia.com>
2023-11-14 14:29:41 +0100

    Boid rules in but not working

--- Kernmodule-AI-Games-Boids/Assets/Scripts/Boid.cs
+++ Kernmodule-AI-Games-Boids/Assets/Scripts/Boid.cs
@@ -5,46 +5,6 @@ using UnityEngine;
 public class Boid : MonoBehaviour
 {
     public Vector3 Velocity = Vector3.zero;
-    public List<Transform> NeighborsInRange;
+    public Vector3 Position = Vector3.zero;
 
-    public void UpdatePosition(Vector3 _pos, float _distance)
-    {
-        transform.position = _pos;
-        transform.rotation = Quaternion.LookRotation(Vector3.Normalize(Velocity));
-        Velocity = SteerSeperation(_distance);
-    }
-
-    private Vector3 SteerSeperation(float _distance)
-    {
-        Vector3 direction = Vector3.zero;
-        foreach (Transform t in NeighborsInRange)
-        {
-            float ratio = Mathf.Clamp01((t.position - transform.position).magnitude / _distance);
-            direction -= ratio * (t.position - transform.position);
-        }
-
-        return direction.normalized;
-    }
-
-    public void UpdateNeighbors(List<Transform> _listBoids, float _distance)
-    {
-        NeighborsInRange.Clear();
-
-        foreach (Transform t in _listBoids)
-        {
-            if (transform != t)
-            {
-                if (Vector3.Distance(t.position, transform.position) < _distance)
-                {
-                    NeighborsInRange.Add(t);
-                }
-            }
-        }
-    }
-
-
-
-
-
-    // Cohesion, separation, alignment methods...
 }
--- Kernmodule-AI-Games-Boids/Assets/Scripts/BoidManager.cs
+++ Kernmodule-AI-Games-Boids/Assets/Scripts/BoidManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class BoidManager : MonoBehaviour
@@ -19,11 +18,12 @@ public class BoidManager : MonoBehaviour
     {
         SpawnBoids();
     }
-    private void Start()
+
+    private void LateUpdate()
     {
         UpdateBoidPositions();
-        SearchNeighbors();
     }
+
 
     private void SpawnBoids()
     {
@@ -34,6 +34,7 @@ public class BoidManager : MonoBehaviour
             float y = Random.Range(0, FieldSize);
             Vector3 BoidSpawnPos = new Vector3(x, y, z);
             Boid newBoid = Instantiate(BoidPrefab, BoidSpawnPos, Quaternion.identity).GetComponent<Boid>();
+            newBoid.Position = BoidSpawnPos;
             transformList.Add(newBoid.transform);
             boidList.Add(newBoid);
         }
@@ -41,20 +42,117 @@ public class BoidManager : MonoBehaviour
 
     private void UpdateBoidPositions()
     {
-        foreach (Boid boid in boidList)
+        Vector3 Rule1 = new Vector3();
+        Vector3 Rule2 = new Vector3();
+        Vector3 Rule3 = new Vector3();
+        Vector3 Rule4 = new Vector3();
+
+        foreach (Boid b in boidList)
         {
-            Vector3 newPos = boid.transform.position + boid.Velocity * Time.deltaTime;
-            boid.UpdatePosition(newPos, DistancePerBoid);
+            Rule1 = Cohesion(b);
+            Rule2 = Separation(b);
+            Rule3 = Alignment(b);
+            Rule4 = BoundToArea(b);
+            b.Velocity = b.Velocity + Rule1 + Rule2 + Rule3;
+            LimitSpeed(b);
+            b.transform.position = b.Position + b.Velocity;
         }
     }
 
-    private void SearchNeighbors()
+    private Vector3 Cohesion(Boid _CurrentBoid)
     {
-        foreach (Boid boid in boidList)
+        Vector3 Center = new Vector3();
+
+        foreach (Boid b  in boidList)
         {
-            boid.UpdateNeighbors(transformList, DistancePerBoid);
+            if (b.transform.position != Center)
+            {
+                Center += b.transform.position;
+            }
+            Center /= BoidAmount - 1;
+        }
+        return (Center - _CurrentBoid.Position) / 100f;
+    }
+
+    private Vector3 Separation(Boid _CurrentBoid)
+    {
+        Vector3 Center = new Vector3();
+        Center = Vector3.zero;
+
+        foreach(Boid b in boidList)
+        {
+            if (b !=  _CurrentBoid)
+            {
+                if (Vector3.Distance(b.transform.position, _CurrentBoid.Position) < DistancePerBoid)
+                {
+                    Center = Center - (b.transform.position - _CurrentBoid.Position);
+                }
+            }
+        }
+        return Center;
+    }
+
+    private Vector3 Alignment(Boid _CurrentBoid)
+    {
+        Vector3 pvJ = new Vector3();
+
+        foreach (Boid b in boidList)
+        {
+            if (b != _CurrentBoid)
+            {
+                pvJ += b.Velocity;
+            }
+        }
+
+        pvJ /= BoidAmount - 1;
+        return (pvJ - _CurrentBoid.Velocity) / 8;
+    }
+
+    private void LimitSpeed(Boid _CurrentBoid)
+    {
+        float Vlim = 3;
+        Vector3 Temp = new Vector3(Vlim, Vlim, Vlim);
+        float boidVelocityMagnitude = _CurrentBoid.Velocity.magnitude;
+        if (boidVelocityMagnitude > Vlim)
+        {
+            _CurrentBoid.Velocity = (_CurrentBoid.Velocity / boidVelocityMagnitude) * Vlim;
         }
     }
 
-    
+    private Vector3 BoundToArea(Boid _CurrentBoid)
+    {
+        float Xmin = 0f;
+        float Xmax = FieldSize;
+        float Ymin = 0f;
+        float Ymax = FieldSize;
+        float Zmin = 0f;
+        float Zmax = FieldSize;
+        Vector3 BoundingDirection = new Vector3();
+
+        if (_CurrentBoid.transform.position.x < Xmin)
+        {
+            BoundingDirection.x = 10; 
+        } else if (_CurrentBoid.transform.position.x > Xmax)
+        {
+            BoundingDirection.x = -10;
+        }
+
+        if (_CurrentBoid.transform.position.y < Ymin)
+        {
+            BoundingDirection.y = 10;
+        } else if (_CurrentBoid.transform.position.y > Ymax)
+        {
+            BoundingDirection.y = -10;
+        }
+
+        if (_CurrentBoid.transform.position.z < Zmin)
+        {
+            BoundingDirection.z = 10;
+        } else if (_CurrentBoid.transform.position.z  > Zmax)
+        {
+            BoundingDirection.z = -10;
+        }
+
+        return BoundingDirection;
+    }
 }
